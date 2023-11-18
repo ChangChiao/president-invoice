4ce78208b6cf735907e2008ae25201d098f5d9f4
joe <JoeCT_Chang@moxa.com>
2023-11-18 09:00:16 +0800

    feat: add map color config

---
+++ src/app/configs/mapColor.ts
@@ -0,0 +1,15 @@
+export const greenList = [
+  '#B2DFDB',
+  '#80CBC4',
+  '#4DB6AC',
+  '#26A69A',
+  '#00897B',
+];
+export const blueList = ['#B3E0FF', '#81D4FA', '#4FC3F7', '#29B6F6', '#0288D1'];
+export const orangeList = [
+  '#FFE0B2',
+  '#FFCC80',
+  '#FFB74D',
+  '#FFA726',
+  '#FF9800',
+];
