c4617bf0a973b737ebc7a69066f6f8bb7fbb0213
joe <JoeCT_Chang@moxa.com>
2023-11-18 15:45:20 +0800

    feat: add initializeAppFactory

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -6,11 +6,38 @@ import {
 import { appRoutes } from './app.routes';
 import { provideHttpClient } from '@angular/common/http';
 import { IconRegistryService } from './service/icon-registry.service';
+import { AppService } from './service/app.service';
+import { AppComponentStore } from './store/app.state';
+import { EMPTY, catchError, finalize, map, tap } from 'rxjs';
+
+const initializeAppFactory = (
+  service: AppService,
+  store: AppComponentStore
+) => {
+  store.setLoading(true);
+  return () =>
+    service.initService().pipe(
+      map((data) => data),
+      tap(([country, town, village]) => {
+        console.log('ccc', country);
+        store.setMapData({ country, town, village });
+        store.setVoteData({ country, town, village });
+      }),
+      finalize(() => store.setLoading(false)),
+      catchError(() => EMPTY)
+    );
+};
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(appRoutes, withEnabledBlockingInitialNavigation()),
     provideHttpClient(),
+    {
+      provide: APP_INITIALIZER,
+      useFactory: initializeAppFactory,
+      deps: [AppService, AppComponentStore],
+      multi: true,
+    },
     {
       provide: APP_INITIALIZER,
       useFactory: (iconRegistryService: IconRegistryService) => () =>
