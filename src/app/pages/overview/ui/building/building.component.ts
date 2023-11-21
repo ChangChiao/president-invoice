8a7f49629f92811d4a81deb869bea444324df8b8
joe <JoeCT_Chang@moxa.com>
2023-11-21 20:11:41 +0800

    feat: add building animation

--- src/app/pages/overview/ui/building/building.component.ts
+++ src/app/pages/overview/ui/building/building.component.ts
@@ -7,8 +7,16 @@ import { CommonModule } from '@angular/common';
   imports: [CommonModule],
   template: `
     <div class="building">
-      <div class="building-mask"></div>
-      <img class="building-img" src="assets/img/building.png" alt="building" />
+      <div class="building-half-circle"></div>
+      <div class="building-container">
+        <div class="building-mask"></div>
+        <img
+          class="building-img"
+          src="assets/img/building.png"
+          alt="building"
+        />
+        <span> 誰能入主總統府？ </span>
+      </div>
     </div>
   `,
   styleUrls: ['./building.component.scss'],
